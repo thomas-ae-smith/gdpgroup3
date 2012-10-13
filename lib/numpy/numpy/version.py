f52c155b89e318f7ef9ccd98a5389ab91e14bf7a
Skinna <Skinna@localhost>
2012-10-13 18:20:44 +0100

    Added a lib folder, containing libraries required to run the project.

---
+++ lib/numpy/numpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+short_version = '1.8.0'
+version = '1.8.0'
+full_version = '1.8.0.dev-3f10c36'
+git_revision = '3f10c36339c0fe40e72378a990e6b3c5423805fb'
+release = False
+
+if not release:
+    version = full_version
