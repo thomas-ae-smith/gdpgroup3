ad8e2537ec0718c1957d6b2cb7153def56cd0fae
Adam Thomas <adam@adam-thomas.co.uk>
2012-10-17 16:07:54 +0100

    import epg script

--- client/src/system/bootstrap.php
+++ client/src/system/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 ini_set('memory_limit', '1500M');
 
+require_once 'config.php';
 require_once 'Tonic/Autoloader.php';
 require_once __DIR__ . '/../resources/base.php';
 require_once 'rb.php';
 require_once 'common.php';
 
-R::setup('mysql:host=localhost;port=3306;dbname=your4','your4','4uzEse9&nuja6$5r');
-
+set_db('your4');
--- client/src/system/common.php
+++ client/src/system/common.php
@@ -1,1 +1,9 @@
 <?php
+
+require_once 'config.php';
+require_once 'rb.php';
+
+function set_db($conf_name) {
+	global $DB;
+	R::setup($DB[$conf_name]['string'], $DB[$conf_name]['username'], $DB[$conf_name]['password']);
+}
