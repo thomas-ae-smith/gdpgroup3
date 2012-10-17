77e38011d9f10d666543356471ea78679a461cb2
Jim Skinner <jskinner.mail@gmail.com>
2012-10-17 17:35:13 +0100

    Modified programmes_cron to save programme data to the local database along with a programme vector generated by genres from tvdb. The cache files are cached tvdb records to minimize tvdb calls.

--- datastore/programmes_cron.py
+++ datastore/programmes_cron.py
@@ -7,43 +7,97 @@ import pickle
 import time
 
 import mysql.connector
-import numpy
 import tvdb_api
 
-SECONDS_LOOKAHEAD = 60 * 60 # 1 hour
-THETVDB_API_KEY = '45B44486379A2047'
+def get_epg(lookahead=3600):
+	"""Pulls EPG data from the project4 database for the next `lookahead` 
+	seconds, processes it and writes it to the local database"""
 
-def calc_prog_vector(genre, type, rating, name, description):
-	t = tvdb_api.Tvdb()
-	t[name]
-	return numpy.array([0,0,0,0,0,0,0,0,0,0])
+	time_now = int(time.time())
+	query_inqb8r = ('SELECT `key`, `channelID`, `genre`, `type`, '
+						'`duration_min`, `start_TimeStamp`, `rating` '
+					'FROM project4_epg '
+					'WHERE start_TimeStamp BETWEEN {earliestTime} AND {latestTime}'
+					).format(
+						earliestTime=time_now,
+						latestTime=time_now+lookahead)
+	query_warlock = ('INSERT INTO your4(id, channel, vector, length, start_time)'
+			'VALUES (%s, %s, %s, %s, %s)')
 
-time_now = int(time.time())
-query_inqb8r = ('SELECT `key`, `channelID`, `genre`, `type`, '
-					'`duration_min`, `start_TimeStamp`, `rating` '
-				'FROM project4_epg '
-				'WHERE start_TimeStamp BETWEEN {earliestTime} AND {latestTime}'
-				).format(
-					earliestTime=time_now,
-					latestTime=time_now+SECONDS_LOOKAHEAD)
+	try:
+		conn_inqb8r = mysql.connector.connect(user='teamgdp',
+											database='inspirit_inqb8r',
+											password='MountainDew2012',
+											host='77.244.130.51',
+											port=3307)
+		cursor_inqb8r = conn_inqb8r.cursor()
+		cursor_inqb8r.execute(query)
+	except: # If anything goes wrong, close the connection!
+		conn_inqb8r.close()
+		raise
+	conn_inqb8r.close()
 
-conn_inqb8r = mysql.connector.connect(user='teamgdp',
-									database='inspirit_inqb8r',
-									password='MountainDew2012',
-									host='77.244.130.51',
-									port=3307)
-cursor_inqb8r = conn_inqb8r.cursor()
-cursor_inqb8r.execute(query)
-conn_inqb8r.close()
+	try:
+		conn_warlock = mysql.connector.connect(user='your4',
+											password='2zVGP58Z5YttvAxV',
+											database='your4')
+		cursor_warlock = conn_warlock.cursor()
+		for (key, chanID, genre, type, duration, startTime, rating) in cursor_inqb8r:
+			vector = pickle.dumps(calc_prog_vector())
+			cursor.execute(query, (key, chanID, vector, duration, startTime))
+	except:	# If anything goes wrong, close the connection!
+		conn_inqb8r.close()
+		raise
+	conn_inqb8r.close()
 
-query_warlock = ('INSERT INTO your4(id, channel, vector, length, start_time)'
-		'VALUES (%s, %s, %s, %s, %s)')
-conn_warlock = mysql.connector.connect(user='your4',
-									password='2zVGP58Z5YttvAxV',
-									database='your4')
-cursor_warlock = conn_warlock.cursor()
-for (key, chanID, genre, type, duration, startTime, rating) in cursor_inqb8r:
-	vector = pickle.dumps(calc_prog_vector())
-	cursor.execute(query, (key, chanID, vector, duration, startTime))
+_genre_convert = {
+	"Action and Adventure":0,
+	"Animation":1,
+	"Children":2,
+	"Comedy":3,
+	"Documentary":4,
+	"Drama":5,
+	"Game Show":6,
+	"Home and Garden":7,
+	"Mini-Series":8,
+	"News":9,
+	"Reality":10,
+	"Science-Fiction":11,
+	"Fantasy":12,
+	"Soap":13,
+	"Special Interest":14,
+	"Sport":15,
+	"Talk Show":16,
+	"Western":17,
+	"Unclassified":18
+}
 
-conn_inqb8r.close()
+def get_programme_vector(name):
+	"""Given a programme name, returns a vector representiong that programme 
+	to be used by the recommender"""
+	genre_vec = [0] * len(genre_convert)
+	try:
+		tvdb_genres = tvdb[title]['genre']
+
+		for genre in filter(None, tvdb_genres.split('|')):
+			genre_vec[genre_convert[genre.rstrip()]] = 1
+		movies[movieid] = genre_vec
+		if not tvdb_genres:
+			print("Empty genre list returned by tvdb: "+title)
+	except AttributeError:
+		print("No 'genre' attribute returned by tvdb: "+title)
+		genre_vec[genre_convert['Unclassified']] = 1
+	except tvdb_api.tvdb_shownotfound:
+		print("Not found by tvdb: "+title)
+		genre_vec[genre_convert['Unclassified']] = 1
+	except KeyError:
+		print("KeyError returned by tvdb: "+title)
+		genre_vec[genre_convert['Unclassified']] = 1
+	except tvdb_api.tvdb_error:
+		print("Error 'tvdb_error', possibly malformed repsonse: "+title)
+		genre_vec[genre_convert['Unclassified']] = 1
+	except IndexError:
+		print("IndexError returned by tvdb. WTF?!?!?!?: "+title)
+		genre_vec[genre_convert['Unclassified']] = 1
+
+	return genre_vec
