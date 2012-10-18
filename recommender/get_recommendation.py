179caede6e6d32707d539c6b49a3a39e0a3b86e4
Jim Skinner <jskinner.mail@gmail.com>
2012-10-18 17:58:23 +0100

    Moved SVM stuff into its own folder, and added db connectivity and argument parsing to get_recommendation.

--- recommender/get_recommendation.py
+++ recommender/get_recommendation.py
@@ -1,3 +1,27 @@
 from __future__ import print_function
 
+import argparse
+
+import mysql.connector
+
+parser = argparse.ArgumentParser(description="Given a user ID, returns a "
+"channel recoomended for the user to switch to after their current programme "
+"has finished.")
+
+parser.add_argument('user_id', metavar='uid', type=str,
+					help="The ID of a user")
+
+args = parser.parse_args()
+
+query = (	'SELECT `vector` '
+			'FROM `users` '
+			'WHERE `id` = %s')
+
+conn = mysql.connector.connect(user='your4',
+							password='2zVGP58Z5YttvAxV',
+							database='your4')
+cursor = conn.cursor()
+
+cursor.execute(query, args.user_id)
+
 print("1", end='')
