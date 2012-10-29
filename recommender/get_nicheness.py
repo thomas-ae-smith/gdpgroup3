669b38609b16776553635f765208c8deb39ad7b7
Jim Skinner <js40g09@ecs.soton.ac.uk>
2012-10-29 15:52:28 +0000

    Can use get_nicheness to calculate the user nicheness fo a given set of campaign constraints.

---
+++ recommender/get_nicheness.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python2.7
+
+from __future__ import division, print_function
+
+import argparse
+import datetime
+from dateutil.relativedelta import relativedelta
+import json
+
+import mysql.connector
+
+from datastore.credentials import credentials
+
+def get_user_nicheness(ageranges=[], boundingboxes=[], genders=[], occupations=[]):
+	now = datetime.datetime.now().date()
+	age_constraints = (" OR ".join([
+		"(`users`.`dob` BETWEEN '{y1}' AND '{y2}')".format(
+			y1=now-relativedelta(years=upper),
+			y2=now-relativedelta(years=lower))
+		for lower, upper in ageranges])) 
+
+	bb_constraints = (" OR ".join([
+		"(`users`.`long` BETWEEN '{longmin}' AND '{longmax}' "
+		"AND `users`.`lat` BETWEEN '{latmin}' AND '{latmax}')".format(
+			longmin=longmin, longmax=longmax, latmin=latmin, latmax=latmax)
+		for latmin, latmax, longmin, longmax in boundingboxes]))
+
+	if genders:
+		gender_constraints = ("`users`.`gender` "
+			"IN ({occupations})".format(occupations=",".join(occupations)))
+	else:
+		gender_constraints = ""
+
+	if occupations:
+		occupation_constraints = ("`users`.`occupation` "
+			"IN ({occupations})".format(occupations=",".join(occupations)))
+	else:
+		occupation_constraints = ""
+
+	constraints = [age_constraints, bb_constraints, gender_constraints, 
+		occupation_constraints]
+	query = ("SELECT COUNT(`id`) FROM `users` WHERE {}".format(
+			" AND ".join(filter(None, constraints)) or "1"))
+		
+	conn = mysql.connector.connect(**credentials)
+	cursor = conn.cursor()
+
+	cursor.execute(query)
+	users_constrained = cursor.next()[0]
+
+	cursor.execute("SELECT COUNT(`id`) FROM `users` WHERE 1")
+	users_all = cursor.next()[0]
+
+	import pdb; pdb.set_trace()
+
+	return users_constrained / users_all
+
+def get_nicheness(ageranges=[], boundingboxes=[], genders=[], genres=[],
+									occupations=[], programmes=[], times=[]):
+	user_nicheness = get_user_nicheness(ageranges, boundingboxes,
+											genders, occupations)
+
+	return 1
+
+def _init_argparse():
+	parser = argparse.ArgumentParser(description="Given a set of restrictions, "
+		"returns the nicheness of a campaign with those restrictions. Store "
+		"this value in the 'nicheness' attribute of each campaign.")
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('-j', '--json', type=str,
+						help="An input string in json format.")
+	group.add_argument('-t', "--test", action='store_true',
+						help="Runs using an example set of constraints for "
+						"testing purposes.")
+	parser.add_argument('-v', "--verbose", action='store_true',
+						help="Prints more information to stdout.")
+	parser.add_argument('-d', "--debug", action="store_true",
+						help="If true, breaks using pdb in a number of cases.")
+	return parser.parse_args()
+
+# If called from the commandline
+if __name__ == "__main__":
+	args = _init_argparse()
+
+	if args.test:
+		args.json = json.dumps({
+			'ageranges':[],
+			'boundingboxes':[],
+			'genders':[],
+			'genres':[],
+			'occupations':[],
+			'programmes':[],
+			'times':[]
+		})
+
+	input_dict = json.loads(args.json)
+
+	print(get_nicheness(**input_dict), end='')
