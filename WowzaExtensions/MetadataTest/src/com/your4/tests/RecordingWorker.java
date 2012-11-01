2373a2498b1e32ad3ad8d4bad28795c156c8e165
gdp3dexter <gdp3dexter@edge1.soton.ac.uk>
2012-11-01 18:12:56 +0000

    Imported recording agent code and downgraded to JRE6 (Note: Wowza IDE 2 does not support Java 1.7)
    - Removed general alternate minute recorder
    - Updated function to access SQL DB and determine what streams to record
    - Recorded files are named as [rawID].mp4
    - Executed recording tests on server

--- WowzaExtensions/MetadataTest/src/com/your4/tests/RecordingWorker.java
+++ WowzaExtensions/MetadataTest/src/com/your4/tests/RecordingWorker.java
@@ -1,22 +1,15 @@
 package com.your4.tests;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.util.Date;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import com.wowza.wms.amf.AMFPacket;
 import com.wowza.wms.application.IApplicationInstance;
-import com.wowza.wms.stream.IMediaStream;
-import com.wowza.wms.stream.MediaStreamMap;
 import com.wowza.wms.plugin.livestreamrecord.ModuleLiveStreamRecord;
 
 public class RecordingWorker extends Thread {
@@ -25,61 +18,63 @@ public class RecordingWorker extends Thread {
 		public TimerTask mTask;
 		public ModuleLiveStreamRecord rec;
 		public IApplicationInstance appInstance;
-		
-		public RecordingWorker(IApplicationInstance appInst){
+		public HashMap<String, RecordingObj> currentRecordings;
+	    public static final String dbUrl = "jdbc:mysql://77.244.130.51:3307/inspirit_inqb8r";
+	    public static final String dbClass = "com.mysql.jdbc.Driver";
+	    
+	    public RecordingWorker(IApplicationInstance appInst){
+			currentRecordings = new HashMap<String, RecordingObj>();
 			this.appInstance = appInst;
 			rec = new ModuleLiveStreamRecord();
-			mTask = new TimerTask()
-			{
-				String url = "http://152.78.144.19:8086/livestreamrecord";
-				String charset = "UTF-8";
-				String app = "your4";
-				String streamname = "e4.stream";
-				String action = "";
-				String append = "false";
-				String version = "true";
-				String startonkeyframe = "false";
-				String recorddata = "true";
-				String output = "C:\\WowzaMediaServer\\content\\e4Recording.mp4";
-				String format = "2";
+			mTask = new TimerTask(){
+				private ResultSet rs;
 				
-				public void run() 
-				{
-					
-					//Alternate starting and stopping
-					if(action.equalsIgnoreCase("startRecording")){
-						action = "stopRecording";
-					}else{
-						action = "startRecording";
-					}
-										
-					try {
-						String query = String.format("app=%s&streamname=%s&action=%s&append=%s&version=%s&startonkeyframe=%s&recorddata=%s&output=%s&format=%s", 
-						     URLEncoder.encode(app, charset), 
-						     URLEncoder.encode(streamname, charset), 
-						     URLEncoder.encode(action, charset), 
-						     URLEncoder.encode(append, charset), 
-						     URLEncoder.encode(version, charset), 
-						     URLEncoder.encode(startonkeyframe, charset), 
-						     URLEncoder.encode(recorddata, charset), 
-						     URLEncoder.encode(output, charset), 
-						     URLEncoder.encode(format, charset));
+				public void run(){
+                	try {
+						Class.forName("com.mysql.jdbc.Driver");
 						
-						URLConnection connection = new URL(url + "?" + query).openConnection();
-						connection.setRequestProperty("Accept-Charset", charset);
-						InputStream response = connection.getInputStream();
-						int status = ((HttpURLConnection) connection).getResponseCode();
-						System.out.println("Recorder responded: " + status);
-						
-					} catch (UnsupportedEncodingException e) {
+	               		Connection con = DriverManager.getConnection (dbUrl, "teamgdp", "MountainDew2012");
+	                    Statement stmt = con.createStatement();
+	                    rs = stmt.executeQuery("SELECT epg.key, epg.channelID, epg.rawID, "
+	                            + "CONCAT(epg.date, ' ', epg.start_time_GMT) as startTimeStamp, "
+	                            + "DATE_ADD(CONCAT(epg.date, ' ', epg.start_time_GMT), INTERVAL epg.duration_min MINUTE) as end_timestamp "
+	                            + "FROM inspirit_inqb8r.project4_epg as epg WHERE CONCAT(epg.date, ' ', epg.start_time_GMT) "
+	                            + "BETWEEN DATE_SUB('" + Utils.getCurrentTimeStamp() + "', INTERVAL 1 MINUTE) "
+	                            + "AND DATE_ADD('" + Utils.getCurrentTimeStamp() + "', INTERVAL 1 MINUTE);");
+	
+	                    //Stop any completed recordings
+	                    for (String rawID : currentRecordings.keySet()){
+	                        if (currentRecordings.get(rawID).hasEnded()){
+	                            RecordingObj rec = currentRecordings.remove(rawID);
+	                            System.out.println(rec.getRawID());
+	                        }
+	                    }
+	                    
+	                    //Start any new recordings needed
+	                    while (rs.next()) {
+	                        if (!(currentRecordings.containsKey(rs.getString("rawID")))){
+	                            if (Utils.getStreamNameFromChannelID(rs.getLong("channelID")) != null){
+	                                currentRecordings.put(rs.getString("rawID"), new RecordingObj(rs.getLong("channelID"), 
+	                                        rs.getString("rawID"), rs.getInt("key"), rs.getString("startTimeStamp"), rs.getString("end_timestamp")));
+	                                currentRecordings.get(rs.getString("rawID")).sendCommand(RecordingActionEnum.startRecording);
+	                            }
+	                        }
+	                    }
+                	} catch (ClassNotFoundException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					} catch (MalformedURLException e) {
+					} catch (SQLException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+					}finally{
+						if (rs != null){
+							try {
+								rs.close();
+							} catch (SQLException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
 					}
 				}
 			};
