721a037e1d073f024d5710524fcee37875b10b92
Peter West <pw9g09ecs.soton.ac.uk@localhost>
2012-11-04 16:12:41 +0000

    REST service for programme recommendation working :)

--- client/src/resources/programmes.php
+++ client/src/resources/programmes.php
@@ -1,12 +1,36 @@
 <?php
 
 $app->get('/programmes(/)', function() use ($app) {
-	$programmes = R::find('programmes');
-	output_json(array_map(function ($programme) {
-		return array(
+	$userId = $app->request()->get('user');
+	if ($userId) {
+		$user = R::load('users', $userId);
+		if (!$userId) {
+			return notFound('User with that ID not found.');
+		}
+		unset($out);
+		exec('python ../../../recommender/get_recommendation.py -t 1351209900 ' . $user->id, $out);
+		$programmeId = $out[0];
+		$programme = R::load('programmes', $programmeId);
+		output_json(array(array(
 			'id' => $programme->id,
-			'name' => $programme->name
-		);
-	}, array_values($programmes)));
-	//output_json(R::exportAll($programmes));
+			'channel' => $programme->channel,
+			'start' => $programme->start,
+			'nextAdBreakStart' => 0,
+			'nextAdBreakEnds' => 0,
+			'duration' => $programme->length * 60,
+			'name' => $programme->name,
+			'episode' => $programme->episode,
+			'type' => $programme->type
+		)));
+		
+	} else {
+		$programmes = R::find('programmes');
+		output_json(array_map(function ($programme) {
+			return array(
+				'id' => $programme->id,
+				'name' => $programme->name
+			);
+		}, array_values($programmes)));
+		//output_json(R::exportAll($programmes));
+	}
 });
