53985b2c8e775fd5779237688dd2ec2c898de13e
taes1g09 <taes1g09@ecs.soton.ac.uk>
2012-11-11 16:17:04 +0000

    Corrected rogue unescaped character in attribution.

--- client/webroot/js/advertiser/App.js
+++ client/webroot/js/advertiser/App.js
@@ -338,7 +338,7 @@
 				scrollWheelZoom: false
 			})
 			L.tileLayer('http://{s}.tile.cloudmade.com/1b189a705e22441c86cdb384a5bc7837/997/256/{z}/{x}/{y}.png', {
-				attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+				attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://cloudmade.com">CloudMade</a>',
 				maxZoom: 18
 			}).addTo(map);
 			setTimeout(function () {
