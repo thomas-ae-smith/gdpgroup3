b73818444ef5a41ea5979eacaada1c0b8d3f60fd
Peter West <peter.g.west@gmail.com>
2012-11-14 11:50:02 +0000

    Started work on new playlist, which will build up the playlist in advance

--- client/webroot/js/models.js
+++ client/webroot/js/models.js
@@ -26,7 +26,17 @@
 
 	y4.Programmes = Backbone.Collection.extend({
 		url: "http://"+baseUrl+"/api/programmes/",
-		//comparator: nameComparator
+		//comparator: nameComparator,
+		recommendation: function (userId) {
+			var that = this,
+				dfd = $.Deferred();
+			this.programmes.fetch({
+				data: { user: userId }
+			}).done(function () {
+				dfd.reject(); // FIXME
+			}).fail(function () { dfd.reject(); });
+			return dfd;
+		}
 	});
 
 	y4.Broadcast = Backbone.Model.extend({
@@ -44,7 +54,17 @@
 
 	y4.Broadcasts = Backbone.Collection.extend({
 		url: "http://"+baseUrl+"/api/broadcasts/",
-		model: y4.Broadcast
+		model: y4.Broadcast,
+		recommendation: function (userId, startTime) {
+			var that = this,
+				dfd = $.Deferred();
+			broadcasts.fetch({
+				data: { user: userId, startTime: startTime }
+			}).done(function () {
+				dfd.resolve(that.broadcasts.first()); // TODO: check there is a first?
+			}).fail(function () { dfd.reject(); });
+			return dfd;
+		}
 	});
 
 
@@ -65,7 +85,21 @@
 	});
 	y4.Adverts = Backbone.Collection.extend({
 		url: "http://"+baseUrl+"/api/adverts/",
-		model: y4.Advert
+		model: y4.Advert,
+		recommendation: function (userId, programmeId, timelimit) {
+			var that = this,
+				dfd = $.Deferred();
+			this.adverts.fetch({
+				data: {
+					user: userId,
+					programme: programmeId,
+					time_limit: timelimit
+				}
+			}).done(function () {
+				dfd.resolve(that.adverts.first()); // TODO: check there is a first?
+			}).fail(function () { dfd.reject(); });
+			return dfd;
+		}
 	});
 
 
@@ -213,9 +247,6 @@
 				}
 			});
 		},
-		registerFB: function () {
-
-		},
 		loginFB: function () {
 			var that = this,
 				dfd = $.Deferred();
@@ -243,23 +274,208 @@
 		url: 'http://'+baseUrl+'/api/channels/'
 	});
 
-	y4.Playlist = Backbone.Model.extend({
+	y4.AdbreakAdverts = y4.Adverts.extend({
+		initialize: function (models, options) {
+			this.user = options.user;
+			this.programmeId = options.programmeId;
+		},,
+		advertRecommendation: function (broadcast) {
+			return (new y4.Adverts()).recommendation(this.user.id, programmeId, timelimit);
+		}
+		fetchRecommendedAdvert: function (timelimit) {
+			var dfd = $.Deferred(),
+				adverts = new y4.Adverts();
+			adverts.fetch({
+				data: {
+					user: this.user,
+					programme: this.programme,
+					timelimit: timelimit
+				}
+			}).done(function () {
+				dfd.resolve(adverts.first());
+			}).fail(function () { dfd.reject(); });
+			return dfd;
+		}
+		setBreakDuration: function (s) {
+			var that = this,
+				dfd = $.Deferred(),
+				timeRemaining = s - this.totalDuration(),
+				addAdvert = function () {
+					if (timeRemaining <= 0) {
+						dfd.resolve();
+						return;
+					}
+					that.fetchRecommendedAdvert(timeRemaining).done(function () {
+						addAdvert();
+					}).fail(function () {
+						dfd.resolve();
+					});
+				};
+			this.duration = s;
+			addAdvert();
+			return dfd;
+		},
+		totalDuration: function () {
+			return this.reduce(function (memo, model) {
+				return memo + Number(model.get("duration"));
+			}, 0);
+		}
+	});
+
+	y4.ProgrammeSection = Backbone.Model.extend({
+
+	});
+
+	y4.PlaylistItem = Backbone.Model.extend({
+
+	});
+
+	y4.Playlist = Backbone.Collection.extend({
+		model: y4.playlistItem,
 		initialize: function (options) {
 			this.user = options.user;
-			this.broadcasts = new y4.Broadcasts();
-			this.programmes = new y4.Programmes();
-			this.adverts = new y4.Adverts();
-		},
-		start: function (advert, force) {
-			return this.recommend();
+			this.programmes = [];
+			/*this.reset([
+				{ type: "break", item: new y4.AdbreakAdverts() },
+				{ type: "broadcast", item: new y4.ProgrammeSection({ section: 0 }) },
+				{ type: "break", item: new y4.AdbreakAdvert() },
+				{ type: "broadcast", item: new y4.ProgrammeSection({ section: 1 }) },
+				{ type: "break", item: new y4.AdbreakAdvert() },
+				{ type: "vod", section: 1, item: new y4.ProgrammeSection() }
+				// etc
+			];*/
 		},
 
-		showAdvert: function (broadcast) {
-			console.log("Showing advert");
+		fetchBroadcasterTime: function () {
+			var that = this,
+				localNow = (new Date).getTime() / 1000;
+			return $.ajax({
+				url: 'http://'+baseUrl+'/api/time/',
+				dataType: "json",
+				contentType: "application/json"
+			}).done(function (resonse) {
+				that.timeOffset = response.broadcastNow - localNow;
+			});
+		},
+
+		broadcasterTime: function () {
+			var localNow = (new Date).getTime() / 1000;
+			return localNow + this.timeOffset;
+		},
+
+		broadcasterPlaylistEndTime: function () {
+
+		},
+
+		start: function (advert, force) {
 			var that = this,
 				dfd = $.Deferred();
-			that.advertRecommendation(broadcast).done(function (advert) {
-				that.advert(advert).on("finish", function () {
+			this.fetchBroadcastTime().done(function () {
+				that.fill().done(function () {
+					dfd.resolve();
+				});
+			});
+			return this;
+		},
+
+		totalDuration: function () {
+			return _(this.playlist).reduce(function (memo, item) {
+				return memo + Number(item.item.duration);
+			}, 0);
+		},
+
+		programmeCount: function () {
+			this.programmes.length;
+		},
+
+		minPlaylistDuration: 60000, // 1 hour in advance
+		minNumberOfProgrammes: 2,
+
+		// Fill the playlist up to the minimum length or minimum number of programmes
+		fill: function () {
+			var that = this,
+				programmeFetcher = $.Deferred(),
+				dfd = $.Deferred();
+
+			if (this.totalDuration() < this.minPlaylistDuration &&
+					this.programmeCount() < minNumberOfProgrammes) {
+
+				// Try to get a broadcast
+				this.pushBroadcastRecommendation(that.broadcasterPlaylistEndTime()).done(function () {
+					programmeFetcher.resolve()
+				}).fail(function () {
+					// No broadcast? Get a vod
+					that.pushVodRecommendation().done(function () {
+						programmeFetcher.resolve();
+					}).fail(function () { console.error("FIXME: impossible case"); });
+				});
+
+				// Once recommendation has been made, add more to the playlist -- asynchronous recursivity!
+				programmeFetcher.done(function () {
+					that.fill().done(function () {
+						dfd.resolve();
+					});
+				});
+			} else {
+				dfd.resolve();
+			}
+
+			return dfd;
+		},
+
+		pushProgramme: function (programme, breakBeforeDuration) {
+			var that = this,
+				dfd = $.Deferred();
+
+			// Fill in time before start with adverts
+			this.pushAdverts(breakBeforeDuration, programme).done(function () {
+				// Split programmes into sections with breaks for adverts
+				var programmeSections = new y4.ProgrammeSections(undefined, {
+					programme: programme,
+					advertTimings: programme.advertTimings()
+				});
+
+			});
+			// fill in time in adbreaks with adverts
+			// get broadcast for after that broadcast/programme has finished
+
+			this.programmes.push(model);
+
+			that.add({ type: "broadcast", startTime: broadcast.localStartTime() });
+
+			// split programme into sections
+					var sections = new y4.ProgrammeSections(),
+						sectionMap = {},
+						updateAdvertsBetweenSection = function () {
+							var section1, section2;
+							for (var i = 0; i < sections.length - 1; i++) {
+								section1 = sections.at(i)
+							}
+						};
+
+					sections.on("add", function (section) {
+						var item = y4.playlistItem({
+							type: "vod",
+							startTime: y4.now() + that.totalDuration() + section.time,
+							item: section
+						});
+						sectionMap[section.id] = item;
+						that.add(item);
+					}).on("remove", function (section) {
+						sectionMap[section.id].destroy();
+						delete sectionMap[section.id];
+					});
+		},
+
+		pushAdverts: function (timelimit, programme) {
+			console.log("Preparing advert break.");
+			var that = this,
+				dfd = $.Deferred(),
+				programmeId = programme ? programme.id : 0,
+				timelimit = timelimit || 0;
+
+			that.pushAdvertRecommendation(timelimit, programmeId).done(function (advert) {
+				that.pushAdverts(timelimit - advert.duration()).done(function () {
 					dfd.resolve();
 				});
 			}).fail(function () {
@@ -267,6 +483,40 @@
 			});
 			return dfd;
 		},
+
+		recommend: function () {
+			console.log("Fetching a recommendation");
+			var that = this,
+				dfd = $.Deferred();
+
+			this.fillBroadcasts().done(function (broadcast) {
+				broadcast.get("time") - broadcast.get("timenow")
+			})
+
+			this.broadcastRecommendation().done(function (broadcast) {
+				that.hasBroadcastStarted(broadcast);
+				dfd.resolve();
+			}).fail(function () {
+				console.log("No broadcast recommendation - trying programmes");
+				that.programmeRecommendation().done(function (programme) {
+					dfd.resolve();
+					that.programme(programme).on("breakStart", function () {
+
+					}).on("finish", function () {
+						that.recommend();
+					});
+				}).fail(function () {
+					console.log("No programme recommendation - resorting to 2 minute ad break")
+					dfd.resolve();
+					that.showAdverts(null, 120).done(function () {
+						that.recommend();
+					});
+				});
+			});
+
+			return dfd;
+		},
+
 
 		startBroadcast: function (broadcast) {
 			console.log("Starting broadcast");
@@ -296,33 +546,6 @@
 				console.log("Broadcast not yet started - " + (broadcast.secondsTillStart() / 60) + " minutes to start");
 				this.showAdvert(broadcast).done(function () { that.hasBroadcastStarted(broadcast); });
 			}
-		},
-
-		recommend: function () {
-			console.log("Making recommendation");
-			var that = this,
-				dfd = $.Deferred();
-
-			this.broadcastRecommendation().done(function (broadcast) {
-				that.hasBroadcastStarted(broadcast);
-				dfd.resolve();
-			}).fail(function () {
-				that.programmeRecommendation().done(function (programme) {
-					dfd.resolve();
-					that.programme(programme).on("breakStart", function () {
-
-					}).on("finish", function () {
-						that.recommend();
-					});
-				}).fail(function () {
-					dfd.resolve();
-					that.showAdvert().done(function () {
-						that.recommend();
-					});
-				});
-			});
-
-			return dfd;
 		},
 
 		broadcast: function (broadcast) {
@@ -371,38 +594,24 @@
 			return o;
 		},
 
-		broadcastRecommendation: function () {
+		pushBroadcastRecommendation: function (startTime) {
 			var that = this,
 				dfd = $.Deferred();
-			this.broadcasts.fetch({
-				data: { user: this.user.id }
-			}).done(function () {
-				dfd.resolve(that.broadcasts.first()); // TODO: check there is a first?
-			}).fail(function () { dfd.reject(); });
+			(new y4.Broadcasts()).recommendation(this.user.id, startTime).done(function (broadcast) {
+				that.pushProgramme("broadcast", broadcast, broadcast.broadcasterTime() - that.broadcasterPlaylistEndTime()).done(function () {
+					dfd.resolve(broadcast);
+				});
+			});
 			return dfd;
 		},
-		programmeRecommendation: function () {
+		pushProgrammeRecommendation: function () {
 			var that = this,
 				dfd = $.Deferred();
-			this.programmes.fetch({
-				data: { user: this.user.id }
-			}).done(function () {
-				dfd.reject(); // FIXME
-			}).fail(function () { dfd.reject(); });
-			return dfd;
-		},
-		advertRecommendation: function (broadcast) {
-			var that = this,
-				dfd = $.Deferred();
-			this.adverts.fetch({
-				data: {
-					user: this.user.id,
-					programme: broadcast ? broadcast.get("programme_id") : 0,
-					time_limit: broadcast ? Math.max(1, Math.floor(broadcast.secondsTillStart())) : 0
-				}
-			}).done(function () {
-				dfd.resolve(that.adverts.first()); // TODO: check there is a first?
-			}).fail(function () { dfd.reject(); });
+			(new y4.Programmes()).recommendation(this.user.id).done(function (vod) {
+				that.pushProgramme("vod", vod, 120).done(function () {
+					dfd.resolve(vod);
+				});
+			});
 			return dfd;
 		}
 	});
