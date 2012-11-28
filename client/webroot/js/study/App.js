d2bb27f013f3c269340f16443b96b0e6dde04cae
Peter West <peter.g.west@gmail.com>
2012-11-28 21:06:56 +0000

    Added confirmation screens to study

--- client/webroot/js/study/App.js
+++ client/webroot/js/study/App.js
@@ -39,9 +39,38 @@
 			this.studies = new y4.Studies();
 		},
 		render: function () {
+			var that = this;
 			this.$el.html("").append(this.player.render().el);
 			this.player.$(".skip").hide();
+			if (window.location.hash) {
+
+			} else {
+				this.renderScreen("When you're ready to begin, press <i>Start</i>.", function () {
+					that.player.videoLayer.trigger("finish")
+				});
+			}
 			return this;
+		},
+		renderScreen: function (msg, start, replay) {
+			var $s = $('<div class="screen"><div class="msg">' + msg + '</div></div>');
+			if (start) {
+				var $start = $('<input type="button" value="Start">');
+				$start.on("click", start).
+					on("click", function () {
+						$s.remove();
+					});
+
+				$s.append($start);
+			}
+			if (replay) {
+				var $replay = $('<input type="button" value="Replay">');
+				$replay.on("click", replay).
+					on("click", function () {
+						$s.remove();
+					});
+				$s.append($replay);
+			}
+			this.$el.append($s);
 		},
 		start: function () {
 			var that = this,
@@ -65,7 +94,7 @@
 
 				var currRound = timeMode,
 					roundCount = 0,
-					stage = 2; // 1: instructions, 2: round 1, 3: instructions, 4: round 2, 5: instructions, 6: finish
+					stage = 1; // 1: instructions, 2: round 1, 3: instructions, 4: round 2, 5: instructions, 6: finish
 
 				that.player.videoLayer.on("finish", function () {
 					console.log(stage)
@@ -74,16 +103,29 @@
 						that.player.setProgramme(instructionals[0]);
 						stage++;
 						break;
-					case 3:
+					case 2:
+					case 5:
+						that.renderScreen("When you're ready to begin, press <i>Start</i>.<br>Press <i>Replay</i> to watch these instructions again.", function () {
+							stage++;
+							that.player.videoLayer.trigger("finish");
+						}, function () {
+							stage--;
+							that.player.videoLayer.trigger("finish");
+						});
+					case 4:
 						that.player.setProgramme(instructionals[1]);
 						stage++;
 						break;
-					case 5:
+					case 6:
 						that.player.setProgramme(instructionals[2]);
 						stage++;
 						break;
-					case 2:
-					case 4:
+					case 8:
+						that.player.setProgramme(instructionals[3]);
+						stage++;
+						break;
+					case 3:
+					case 7:
 						var n = Math.floor(Math.random() * that.roundAdverts[currRound].length),
 							advert = that.roundAdverts[currRound].at(n);
 						that.study.save({
@@ -95,12 +137,13 @@
 							currRound = 1 - currRound;
 							roundCount++;
 							stage++;
-							that.player.trigger("finish");
+							that.player.videoLayer.trigger("finish");
 						}, timeModes[timeMode][roundCount] * 60000);
+						
 						break;
 					}
 					
-				}).trigger("finish");
+				});
 
 			}).fail(function () {
 				$("body").html("Could not fetch adverts.");
